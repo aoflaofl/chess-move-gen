27441a58ec674fbbe360e674cd3c66cc7f743fa4
Gene Johannsen <aofl@yahoo.com>
2018-04-20 13:13:17 -0700

    Added reading FEN positions from a file.

--- src/com/spamalot/chess/movegen/ChessMoveGen.java
+++ src/com/spamalot/chess/movegen/ChessMoveGen.java
@@ -1,5 +1,11 @@
 package com.spamalot.chess.movegen;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Entry class for Chess Move Generator program.
  * 
@@ -18,9 +24,27 @@ public final class ChessMoveGen {
    *          command line
    */
   public static void main(final String[] args) {
-    ChessBoard b = new ChessBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
-    System.out.println(b);
+    File file = new File("./fen.txt");
+
+    BufferedReader br;
+    try {
+      br = new BufferedReader(new FileReader(file));
+      String st;
+      try {
+        while ((st = br.readLine()) != null) {
+          System.out.println(st);
+          ChessBoard b = new ChessBoard(st);
+          System.out.println(b);
+        }
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
   }
 
 }
