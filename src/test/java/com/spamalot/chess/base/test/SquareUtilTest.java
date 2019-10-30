6af7ca83254aefcdd6a0bfdf177bac9d18d88b7e
Gene Johannsen <gejohann@cisco.com>
2019-10-30 16:47:29 -0700

    Handle compiler warnings.

--- src/test/java/com/spamalot/chess/base/test/SquareUtilTest.java
+++ src/test/java/com/spamalot/chess/base/test/SquareUtilTest.java
@@ -1,8 +1,9 @@
 package com.spamalot.chess.base.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -12,106 +13,112 @@ import java.util.Set;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.spamalot.chess.board.SquareUtil;
 
+@SuppressWarnings("static-method")
 public class SquareUtilTest {
-	private SquareUtil squ = SquareUtil.valueOf(1, 4);
+  private static final Logger LOGGER = LoggerFactory.getLogger(SquareUtilTest.class);
 
-	/** For testing exceptions. */
-	@Rule
-	public final ExpectedException exception = ExpectedException.none();
+  private SquareUtil squ = SquareUtil.valueOf(1, 4);
 
-	@Test
-	public void testGetFile() {
-		assertEquals(1, this.squ.getFile());
-	}
+  /** For testing exceptions. */
+  @Rule
+  public final ExpectedException exception = ExpectedException.none();
 
-	@Test
-	public void testGetRank() {
-		assertEquals(4, this.squ.getRank());
-	}
+  @Test
+  public void testGetFile() {
+    assertEquals(1, this.squ.getFile());
+  }
 
-	@Test
-	public void testValueOfNotNull() {
-		SquareUtil a = SquareUtil.valueOf(1, 2);
-		org.junit.Assert.assertNotNull(a);
-	}
+  @Test
+  public void testGetRank() {
+    assertEquals(4, this.squ.getRank());
+  }
 
-	@Test
-	public void testValueOfRange() {
-		this.exception.expect(IllegalArgumentException.class);
-		this.exception.expectMessage("File is -1.  Must be between 0 and 7 inclusive.");
+  @Test
+  public void testValueOfNotNull() {
+    SquareUtil a = SquareUtil.valueOf(1, 2);
+    org.junit.Assert.assertNotNull(a);
+  }
 
-		SquareUtil.valueOf(-1, 2);
-	}
+  @Test
+  public void testValueOfRange() {
+    this.exception.expect(IllegalArgumentException.class);
+    this.exception.expectMessage("File is -1.  Must be between 0 and 7 inclusive.");
 
-	@Test
-	public void testValueOfOutOfRange() {
-		try {
-			SquareUtil.valueOf(1, 29);
-			org.junit.Assert.fail("Should have gotten an exception.");
-		} catch (IllegalArgumentException e) {
-			// Expected.
-		}
+    SquareUtil.valueOf(-1, 2);
+  }
 
-		try {
-			SquareUtil.valueOf(1, -5);
-			org.junit.Assert.fail("Should have gotten an exception.");
-		} catch (IllegalArgumentException e) {
-			// Expected.
-		}
+  @Test
+  public void testValueOfOutOfRange() {
+    try {
+      SquareUtil.valueOf(1, 29);
+      org.junit.Assert.fail("Should have gotten an exception.");
+    } catch (IllegalArgumentException e) {
+      LOGGER.info("Received expected Exception {}", e.getMessage());
+    }
 
-		try {
-			SquareUtil.valueOf(10, 2);
-			org.junit.Assert.fail("Should have gotten an exception.");
-		} catch (IllegalArgumentException e) {
-			// Expected.
-		}
+    try {
+      SquareUtil.valueOf(1, -5);
+      org.junit.Assert.fail("Should have gotten an exception.");
+    } catch (IllegalArgumentException e) {
+      LOGGER.info("Received expected Exception {}", e.getMessage());
+    }
 
-		try {
-			SquareUtil.valueOf(-6, 2);
-			fail("Should have gotten an exception.");
-		} catch (IllegalArgumentException e) {
-			// Expected.
-		}
-	}
+    try {
+      SquareUtil.valueOf(10, 2);
+      org.junit.Assert.fail("Should have gotten an exception.");
+    } catch (IllegalArgumentException e) {
+      LOGGER.info("Received expected Exception {}", e.getMessage());
+    }
 
-	@Test
-	public void testToString() {
-		assertNotNull(SquareUtil.valueOf(4, 4).toString());
-	}
+    try {
+      SquareUtil.valueOf(-6, 2);
+      fail("Should have gotten an exception.");
+    } catch (IllegalArgumentException e) {
+      LOGGER.info("Received expected Exception {}", e.getMessage());
+    }
+  }
 
-	@Test
-	public void testEquals() {
-		assertEquals(SquareUtil.valueOf(4, 4), SquareUtil.valueOf(4, 4));
-		assertNotEquals(SquareUtil.valueOf(4, 4), SquareUtil.valueOf(1, 2));
+  @Test
+  public void testToString() {
+    assertNotNull(SquareUtil.valueOf(4, 4).toString());
+  }
 
-		assertNotEquals(SquareUtil.valueOf(4, 4), SquareUtil.valueOf(4, 2));
-		assertNotEquals(SquareUtil.valueOf(4, 4), SquareUtil.valueOf(2, 4));
+  @Test
+  public void testEquals() {
+    assertEquals(SquareUtil.valueOf(4, 4), SquareUtil.valueOf(4, 4));
+    assertNotEquals(SquareUtil.valueOf(4, 4), SquareUtil.valueOf(1, 2));
 
-		assertNotEquals(SquareUtil.valueOf(4, 4), null);
-		assertNotEquals(SquareUtil.valueOf(4, 4), "Hello, World!");
-	}
+    assertNotEquals(SquareUtil.valueOf(4, 4), SquareUtil.valueOf(4, 2));
+    assertNotEquals(SquareUtil.valueOf(4, 4), SquareUtil.valueOf(2, 4));
 
-	@Test
-	public void testHash() {
-		// Should be 64 different hash values.
-		Set<Integer> x = new HashSet<>();
-		for (int file = 0; file < 8; file++) {
-			for (int rank = 0; rank < 8; rank++) {
-				System.out.println(SquareUtil.valueOf(file, rank).hashCode());
-				x.add(SquareUtil.valueOf(file, rank).hashCode());
-			}
-		}
+    assertNotEquals(SquareUtil.valueOf(4, 4), null);
+    assertNotEquals(SquareUtil.valueOf(4, 4), "Hello, World!");
+  }
 
-		assertEquals(64, x.size());
-	}
+  @SuppressWarnings("boxing")
+  @Test
+  public void testHash() {
+    // Should be 64 different hash values.
+    Set<Integer> x = new HashSet<>();
+    for (int file = 0; file < 8; file++) {
+      for (int rank = 0; rank < 8; rank++) {
+        System.out.println(SquareUtil.valueOf(file, rank).hashCode());
+        x.add(SquareUtil.valueOf(file, rank).hashCode());
+      }
+    }
 
-	@Test
-	public void testEqualsObject() {
-		assertTrue(this.squ == SquareUtil.valueOf(1, 4));
+    assertEquals(64, x.size());
+  }
 
-		assertFalse(this.squ == SquareUtil.valueOf(4, 1));
-	}
+  @Test
+  public void testEqualsObject() {
+    assertTrue(this.squ == SquareUtil.valueOf(1, 4));
+
+    assertFalse(this.squ == SquareUtil.valueOf(4, 1));
+  }
 }
